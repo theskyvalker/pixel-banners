54c1eb869705e82189f3e01eefab83c0b9a4865a
TheSkyvalkeR <theskyvalker10@gmail.com>
2024-01-29 09:14:04 +0800

    add download step to convert script

--- scripts/convert_json_to_images.py
+++ scripts/convert_json_to_images.py
@@ -1,6 +1,7 @@
 import json
 import base64
 import os
+import requests
 
 def save_image(img_data, img_type, folder_path, name):
     for file_type in ["png", "jpeg", "gif"]:
@@ -11,6 +12,16 @@ def save_image(img_data, img_type, folder_path, name):
                 img_file.write(base64.b64decode(image_data))
             return
     print(f"Unsupported image format for {name}")
+
+def fetch_and_save_json(url, path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(path, 'w') as file:
+            json.dump(response.json(), file)
+        return True
+    else:
+        print(f"Failed to fetch JSON data: HTTP {response.status_code}")
+        return False
         
 def save_images_from_json(json_file):
     
@@ -29,4 +40,8 @@ def save_images_from_json(json_file):
 
 if __name__ == "__main__":
     json_file_path = '../json/pixel_banners_traits.json'
-    save_images_from_json(json_file_path)
+    url_path = 'https://1337-traits-builder.vercel.app/banners/traits'
+    if fetch_and_save_json(url_path, json_file_path):
+        print("Saved updated JSON to file.")
+        save_images_from_json(json_file_path)
+        print("Saved images to disk.")
